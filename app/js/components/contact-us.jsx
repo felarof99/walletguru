3150b113b4e02eccf3849c2301228e682f0eee40
Nithin Sonti <nithin.sonti@gmail.com>
2016-03-07 20:17:35 +0530

    [contact-us] added mailto

--- app/js/components/contact-us.jsx
+++ app/js/components/contact-us.jsx
@@ -3,9 +3,9 @@ var React = require('react');
 module.exports = React.createClass({
     render: function(){
         return <div className="flex-col-start-center">
-            <button className="mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect" style={{background: "#ffffff"}}>
+            <a href="mailto:nithin.sonti@gmail.com" className="mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect" style={{background: "#ffffff"}}>
                 Contact us
-            </button>
+            </a>
             <h4 style={{color: "#ffffff"}}>(We'll be thrilled to give you a pitch)</h4>
         </div>
     },
