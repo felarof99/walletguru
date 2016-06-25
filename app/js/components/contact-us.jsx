3a4331e703798a0dd33acb81eecf9e1d14db1c30
Nithin Sonti <nithin.sonti@gmail.com>
2016-06-25 22:30:52 +0530

    [many files] changed desc to localized expense manager, added link to cnbc video

--- app/js/components/contact-us.jsx
+++ app/js/components/contact-us.jsx
@@ -3,10 +3,11 @@ var React = require('react');
 module.exports = React.createClass({
     render: function(){
         return <div className="flex-col-start-center">
-            <a href="mailto:nithin.sonti@gmail.com" className="mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect" style={{background: "#ffffff"}}>
-                Contact us
+            <h4 style={{color: "#ffffff"}}>Check out our beautiful Android App</h4>
+
+            <a href="http://bit.ly/walletguru" className="mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect" style={{background: "#ffffff"}}>
+                Install from here!
             </a>
-            <h4 style={{color: "#ffffff"}}>(We'll be thrilled to give you a pitch)</h4>
         </div>
     },
 });
--- app/js/components/description.jsx
+++ app/js/components/description.jsx
@@ -3,7 +3,7 @@ var React = require('react');
 module.exports = React.createClass({
     render: function(){
         return <div className="flex-row-center-center">
-            <h3 style={{color: "#ffffff", width: 500}}>Contact Us, we'll be thrilled to give you a pitch.</h3>
+            <h3 style={{color: "#ffffff", width: 500}}>Our first product is a localized expense manager for India! Its available in English, Hindi, Tamil and more. </h3>
         </div>
     },
 });
